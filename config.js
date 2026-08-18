3944767d21bff0de48f5a18bc39f0f98d4544380
carloyuenphotography-tech <carloyuenphotography@gmail.com>
2026-08-18 19:29:35 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ const CONFIG = {
   FT_SHEET_ID: "1ultOcsf8s4bFj7CrqyD_DlN4hOZ8ekK7ip4czQQl5Ms",
   MONTHLY_SHEET_ID: "1RQAj3owFnsEn53UZ41EPUErIFJBywJvvW9BKMOUCquI",
   PROJECTS_SHEET_ID: "1ultOcsf8s4bFj7CrqyD_DlN4hOZ8ekK7ip4czQQl5Ms", 
+  PROCEDURES_SHEET_ID: "1RQAj3owFnsEn53UZ41EPUErIFJBywJvvW9BKMOUCquI", // 👈 這裡補上了逗號
 
   // 各分頁的 GID
   GIDS: {
@@ -20,15 +21,17 @@ const CONFIG = {
     HABIT_LOGS: '1991677412',
     FT: "1351247680",
     MONTHLY: "1986460880",
-    PROJECTS: "1071938293" 
+    PROJECTS: "1071938293",
+    PROCEDURES: "284986950" 
   },
 
   API_URLS: {
-    DAILY: 'https://script.google.com/macros/s/AKfycbwfZp2dJaEO4QEHx8dYbZifmMBZHCgnmVQElu7sNSGMa7st8V3IKYQeYB3oeNCLup7T/exec',
+    DAILY: 'https://script.google.com/macros/s/AKfycbxN5EhMdY4pR-EXtKmA3S2F8ysifhFQtYz_kHRrxtkQ9pDxbzFBWLjL3ePSN_l7oUq6/exec',
     IDEAS: 'https://script.google.com/macros/s/AKfycby2i-JNjupBPLaWuASqnUmezJRwUdQNBT2Gzsi27r1TW9kcdb-DaMwnsrYZqdv2VBhU/exec',
     HABIT: 'https://script.google.com/macros/s/AKfycbz72-QL780wO-NVLsszPusQN3WGqCCeb-vn0yZEI8I6dgGwFaawpJtbUATozrE631rE/exec',
     FT: 'https://script.google.com/macros/s/AKfycbwnpWBweDBZoEvHukGHitf0yq3ycBLfF_YpYTCsPhgz-H5snw9rfgEdgGIv7sbMETyVYA/exec',
     PROJECTS: 'https://script.google.com/macros/s/AKfycby2i-JNjupBPLaWuASqnUmezJRwUdQNBT2Gzsi27r1TW9kcdb-DaMwnsrYZqdv2VBhU/exec', 
-    MONTHLY: 'https://script.google.com/macros/s/AKfycbwfZp2dJaEO4QEHx8dYbZifmMBZHCgnmVQElu7sNSGMa7st8V3IKYQeYB3oeNCLup7T/exec'
+    MONTHLY: 'https://script.google.com/macros/s/AKfycbxN5EhMdY4pR-EXtKmA3S2F8ysifhFQtYz_kHRrxtkQ9pDxbzFBWLjL3ePSN_l7oUq6/exec',
+    PROCEDURES: 'https://script.google.com/macros/s/AKfycbxN5EhMdY4pR-EXtKmA3S2F8ysifhFQtYz_kHRrxtkQ9pDxbzFBWLjL3ePSN_l7oUq6/exec'
   }
 };
