4badd30546c2e91642123d8f3b807badebbeb435
carloyuenphotography-tech <carloyuenphotography@gmail.com>
2026-08-24 12:42:42 +0800

    Update config.js

--- friends_memo/config.js
+++ friends_memo/config.js
@@ -1,7 +1,7 @@
 // config.js - 系統全域設定檔
 const APP_CONFIG = {
     // 請將下方引號內換成你的 Google Apps Script 網頁應用程式網址
-    GAS_URL: "https://script.google.com/macros/s/AKfycbz1_Vxa4JLwM1kigvLDZoseZJkK0uZk1oTbI9oND-26uUNLa0ppCmJb8lcH9-ReI8RV/exec", 
+    GAS_URL: "https://script.google.com/macros/s/AKfycbzt3WuchXnGZoij-scyftxbdynCyXCPkglBL1LlFIbgK39BPafj7xOHXyuqqWuOhvJb/exec", 
     
     // 以下為選擇性記錄的 Google Sheets 相關參數（方便日後查閱或擴充）
     SHEET_ID: "12NNNb_tyS0jUcXe8bXTds8FhqqXkkrx19NU00duc2Qc",
