55f696522d034a00fad03ed430f92fe2e446a3e7
carloyuenphotography-tech <carloyuenphotography@gmail.com>
2026-08-28 20:56:45 +0800

    Update config.js

--- finance/config.js
+++ finance/config.js
@@ -1,12 +1,14 @@
 // finance/config.js
 
 const CONFIG = {
-    // 貼上剛剛發佈的 Google Apps Script 部署 URL
+    // 貼上發佈的 Google Apps Script 部署 URL
     gasUrl: "https://script.google.com/macros/s/AKfycbz_dU-9n00eBA8EoSahFfcIVu1hxcS_g4TIovKNiCIEFhy42vBFJVJHc_RKqhcBkIcakQ/exec",
     
-    // 紀錄用：你的 Google Sheet ID (可選填)
-    sheetId: "d/1-1xCCblZ3JkoGWHJ4LUtVIuIzbDktw1ukBOjApEVbzA"
+    // 紀錄用：你的 Google Sheet ID
+    sheetId: "1-1xCCblZ3JkoGWHJ4LUtVIuIzbDktw1ukBOjApEVbzA",
+    
     // 各分頁的 GID
-  GIDS: {
-    FINANCE: '0',
+    GIDS: {
+        FINANCE: '0'
+    }
 };
