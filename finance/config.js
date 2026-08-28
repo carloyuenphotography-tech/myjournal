5d92f6fa7bacee533993b109cf5188a4f6e29cb2
carloyuenphotography-tech <carloyuenphotography@gmail.com>
2026-08-28 20:51:50 +0800

    Create config.js

---
+++ finance/config.js
@@ -0,0 +1,9 @@
+// finance/config.js
+
+const CONFIG = {
+    // 貼上剛剛發佈的 Google Apps Script 部署 URL
+    gasUrl: "https://script.google.com/macros/s/AKfycbz_dU-9n00eBA8EoSahFfcIVu1hxcS_g4TIovKNiCIEFhy42vBFJVJHc_RKqhcBkIcakQ/exec",
+    
+    // 紀錄用：你的 Google Sheet ID (可選填)
+    sheetId: "d/1-1xCCblZ3JkoGWHJ4LUtVIuIzbDktw1ukBOjApEVbzA"
+};
